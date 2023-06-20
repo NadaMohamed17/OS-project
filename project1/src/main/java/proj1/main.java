bdbcfa43878b828730f7c8c8a987c5b4da078b95
NadaMohamed17 <96050120+NadaMohamed17@users.noreply.github.com>
2023-06-20 20:34:49 +0300

    Add files via upload

---
+++ project1/src/main/java/proj1/main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proj1;
+
+/**
+ *
+ * @author tony
+ */
+public class main {
+    public static void main(String[] args) {
+        menu m = new menu();
+        m.setSize(400,360);
+        m.setLocation(400,200);
+        m.setVisible(true);
+    }
+    
+}
